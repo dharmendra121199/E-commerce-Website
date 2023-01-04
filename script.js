a2eaf29adcbc0b464e8194d79866977cb3feefb1
Dharmendra <dharmendra121199@gmail.com>
2023-01-04 08:46:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', function() {
+        nav.classList.add('ctive');
+    })
+}
+if (close) {
+    close.addEventListener('click', function() {
+        nav.classList.remove('ctive');
+    })
+}
